af3d5ce973b4db2704778cbcbcb415a00e0977e9
bachn06 <bachn315@gmail.com>
2022-08-09 22:20:51 +0700

    split be, fe

--- minimartdemo/src/Layout/components/AdminHomePage/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AdminHomePage() {
-	return <div>Admin PAge</div>;
-}
-
-export default AdminHomePage;
--- minimartdemo/src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export const findItemById = (id, list) => {
-	return list.find((item) => item._id === id);
-};
