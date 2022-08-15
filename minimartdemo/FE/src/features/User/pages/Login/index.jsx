c471fc55bba1e1fd8cc2403077ba88570b69c4e9
Nguyen Huu Giang <nguyenhuugiangtb08012001@gmail.com>
2022-08-15 20:34:06 +0700

    update login & register

--- minimartdemo/FE/src/features/User/pages/Login/index.jsx
+++ minimartdemo/FE/src/features/User/pages/Login/index.jsx
@@ -12,150 +12,150 @@ import { toast } from "react-toastify";
 import { Container } from "reactstrap";
 
 function Login() {
-	const [isForgetPassword, setIsForgetPassword] = useState(false);
-	const [isResetPassword, setIsResetPassword] = useState(false);
-	const [resetCode, setResetCode] = useState(null);
-	const dispatch = useDispatch();
-	const history = useHistory();
+  const [isForgetPassword, setIsForgetPassword] = useState(false);
+  const [isResetPassword, setIsResetPassword] = useState(false);
+  const [resetCode, setResetCode] = useState(null);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-	const onLoginSubmit = (data) => {
-		const login = async () => {
-			try {
-				const result = await dispatch(userLogin(data));
-				unwrapResult(result);
-				history.push("/");
-			} catch (error) {
-				throw error;
-			}
-		};
+  const handleLogin = (data) => {
+    const login = async () => {
+      try {
+        const result = await dispatch(userLogin(data));
+        unwrapResult(result);
+        history.push("/");
+      } catch (error) {
+        throw error;
+      }
+    };
 
-		toast.promise(login, {
-			pending: "Đang xử lý",
-			success: "Đăng nhập thành công",
-			error: {
-				render({ data }) {
-					return `Đăng nhập thất bại ${data.message}`;
-				},
-			},
-		});
-	};
+    toast.promise(login, {
+      pending: "Đang xử lý",
+      success: "Đăng nhập thành công",
+      error: {
+        render({ data }) {
+          return `Đăng nhập thất bại ${data.message}`;
+        },
+      },
+    });
+  };
 
-	const onForgetPasswordSubmit = (data) => {
-		const forgetPassword = async () => {
-			try {
-				const response = await userApi.forgetPassword(data);
-				setIsResetPassword(true);
-				setResetCode(response.data.resetCode);
-			} catch (error) {
-				throw error.response.data;
-			}
-		};
+  const handleForgetPassword = (data) => {
+    const forgetPassword = async () => {
+      try {
+        const response = await userApi.forgetPassword(data);
+        setIsResetPassword(true);
+        setResetCode(response.data.resetCode);
+      } catch (error) {
+        throw error.response.data;
+      }
+    };
 
-		toast.promise(forgetPassword, {
-			pending: "Đang xử lý",
-			success: "Nhâp mật khẩu mới",
-			error: {
-				render({ data }) {
-					return `${data.message}`;
-				},
-			},
-		});
-	};
+    toast.promise(forgetPassword, {
+      pending: "Đang xử lý",
+      success: "Nhâp mật khẩu mới",
+      error: {
+        render({ data }) {
+          return `${data.message}`;
+        },
+      },
+    });
+  };
 
-	const onResetPasswordSubmit = (data) => {
-		const resetPassword = async () => {
-			try {
-				await userApi.resetPassword({
-					...data,
-					resetCode,
-				});
-				setIsForgetPassword(false);
-			} catch (error) {
-				throw error.response.data;
-			}
-		};
+  const handleResetPassword = (data) => {
+    const resetPassword = async () => {
+      try {
+        await userApi.resetPassword({
+          ...data,
+          resetCode,
+        });
+        setIsForgetPassword(false);
+      } catch (error) {
+        throw error.response.data;
+      }
+    };
 
-		toast.promise(resetPassword, {
-			pending: "Đang xử lý",
-			success: "Đổi mật khẩu thành công",
-			error: {
-				render({ data }) {
-					return `${data.message}`;
-				},
-			},
-		});
-	};
+    toast.promise(resetPassword, {
+      pending: "Đang xử lý",
+      success: "Đổi mật khẩu thành công",
+      error: {
+        render({ data }) {
+          return `${data.message}`;
+        },
+      },
+    });
+  };
 
-	const defaultLoginValues = {
-		email: "",
-		password: "",
-	};
+  const defaultLoginValues = {
+    email: "",
+    password: "",
+  };
 
-	const defaultForgetPasswordValues = {
-		email: "",
-	};
+  const defaultForgetPasswordValues = {
+    email: "",
+  };
 
-	const defaultResetPasswordValues = {
-		resetCode: "",
-		resetPassword: "",
-		confirmResetPassword: "",
-	};
+  const defaultResetPasswordValues = {
+    resetCode: "",
+    resetPassword: "",
+    confirmResetPassword: "",
+  };
 
-	const openFormForget = () => {
-		setIsForgetPassword(true);
-	};
+  const openFormForget = () => {
+    setIsForgetPassword(true);
+  };
 
-	const goBack = () => {
-		setIsForgetPassword(false);
-		setIsResetPassword(false);
-	};
+  const goBack = () => {
+    setIsForgetPassword(false);
+    setIsResetPassword(false);
+  };
 
-	return (
-		<div className='authen'>
-			<div className='authen__header'>
-				<Container>
-					<div className='d-flex align-items-md-center justify-content-md-between authen__header--wrap'>
-						<div className='d-flex align-items-center'>
-							<Link to='/'>
-								<img src={images.LOGO_B} alt='logo' />
-							</Link>
-							<h1 className='authen__header__title'>Đăng nhập</h1>
-						</div>
-						<p className='authen__header__text'>
-							Đăng nhập tài khoản của bạn !
-						</p>
-					</div>
-				</Container>
-			</div>
-			<div className='authen__content'>
-				<Container>
-					{!isForgetPassword ? (
-						<LoginForm
-							onSubmit={onLoginSubmit}
-							defaultValues={defaultLoginValues}
-							forgetClick={openFormForget}
-						/>
-					) : (
-						<>
-							{isResetPassword ? (
-								<ResetPasswordForm
-									onSubmit={onResetPasswordSubmit}
-									defaultValues={defaultResetPasswordValues}
-									goBack={goBack}
-								/>
-							) : (
-								<ForgetPasswordForm
-									onSubmit={onForgetPasswordSubmit}
-									defaultValues={defaultForgetPasswordValues}
-									goBack={goBack}
-								/>
-							)}
-						</>
-					)}
-				</Container>
-			</div>
-		</div>
-	);
+  return (
+    <div className="authen">
+      <div className="authen__header">
+        <Container>
+          <div className="d-flex align-items-md-center justify-content-md-between authen__header--wrap">
+            <div className="d-flex align-items-center">
+              <Link to="/">
+                <img src={images.LOGO_B} alt="logo" />
+              </Link>
+              <h1 className="authen__header__title">Đăng nhập</h1>
+            </div>
+            <p className="authen__header__text">
+              Đăng nhập tài khoản của bạn !
+            </p>
+          </div>
+        </Container>
+      </div>
+      <div className="authen__content">
+        <Container>
+          {!isForgetPassword ? (
+            <LoginForm
+              onSubmit={handleLogin}
+              defaultValues={defaultLoginValues}
+              forgetClick={openFormForget}
+            />
+          ) : (
+            <>
+              {isResetPassword ? (
+                <ResetPasswordForm
+                  onSubmit={handleResetPassword}
+                  defaultValues={defaultResetPasswordValues}
+                  goBack={goBack}
+                />
+              ) : (
+                <ForgetPasswordForm
+                  onSubmit={handleForgetPassword}
+                  defaultValues={defaultForgetPasswordValues}
+                  goBack={goBack}
+                />
+              )}
+            </>
+          )}
+        </Container>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- minimartdemo/FE/src/features/User/pages/Register/index.jsx
+++ minimartdemo/FE/src/features/User/pages/Register/index.jsx
@@ -6,65 +6,60 @@ import { toast } from "react-toastify";
 import { Container } from "reactstrap";
 
 function Register() {
-	const history = useHistory();
-	const onSubmit = (data) => {
-		const register = async () => {
-			try {
-				await userApi.register(data);
+  const history = useHistory();
+  const handleSubmit = (data) => {
+    const handleRegister = async () => {
+      try {
+        await userApi.register(data);
 
-				history.push("/user/login");
-			} catch (error) {
-				throw error;
-			}
-		};
+        history.push("/user/login");
+      } catch (error) {
+        throw error;
+      }
+    };
 
-		toast.promise(register, {
-			pending: "Đang xử lý",
-			success: "Đăng ký thành công",
-			error: {
-				render: ({ data }) => {
-					const { message } = data.response.data;
-					return `Đăng kí thất bại với lỗi: ${message}`;
-				},
-			},
-		});
-	};
+    toast.promise(handleRegister, {
+      pending: "Đang xử lý",
+      success: "Đăng ký thành công",
+      error: {
+        render: ({ data }) => {
+          const { message } = data.response.data;
+          return `Đăng kí thất bại với lỗi: ${message}`;
+        },
+      },
+    });
+  };
 
-	const defaultValues = {
-		surname: "",
-		name: "",
-		phone: "",
-		email: "",
-		password: "",
-		confirmPassword: "",
-	};
-	return (
-		<div className='authen'>
-			<div className='authen__header'>
-				<Container>
-					<div className='d-flex align-items-center justify-content-between authen__header--wrap'>
-						<div className='d-flex align-items-center'>
-							<Link to='/'>
-								<img src={images.LOGO_B} alt='logo' />
-							</Link>
-							<h1 className='authen__header__title'>Đăng ký</h1>
-						</div>
-						<p className='authen__header__text'>
-							Đăng ký tài khoản mới !
-						</p>
-					</div>
-				</Container>
-			</div>
-			<div className='authen__content'>
-				<Container>
-					<RegisterForm
-						onSubmit={onSubmit}
-						defaultValues={defaultValues}
-					/>
-				</Container>
-			</div>
-		</div>
-	);
+  const defaultValues = {
+    surname: "",
+    name: "",
+    phone: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  return (
+    <div className="authen">
+      <div className="authen__header">
+        <Container>
+          <div className="d-flex align-items-center justify-content-between authen__header--wrap">
+            <div className="d-flex align-items-center">
+              <Link to="/">
+                <img src={images.LOGO_B} alt="logo" />
+              </Link>
+              <h1 className="authen__header__title">Đăng ký</h1>
+            </div>
+            <p className="authen__header__text">Đăng ký tài khoản mới !</p>
+          </div>
+        </Container>
+      </div>
+      <div className="authen__content">
+        <Container>
+          <RegisterForm onSubmit={handleSubmit} defaultValues={defaultValues} />
+        </Container>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
